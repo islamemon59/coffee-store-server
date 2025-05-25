25752437d3c9856c141bb736cadbca937115fc2f
islamemon59 <islamemon59@gmail.com>
2025-05-25 17:46:34 +0600

    require formated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
